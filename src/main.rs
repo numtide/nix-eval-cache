4dc152f5241014f21d6a22c3c3cce9d58c892694
Jörg Thalheim <joerg@thalheim.io>
2022-04-08 17:35:39 +0200

    compute hash key

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,9 @@
+use blake2::{Blake2s256, Digest};
 use nix::fcntl::{self, fcntl, FcntlArg, OFlag, FdFlag};
 use nix::unistd;
 use std::io::Write;
 use std::os::unix::io::AsRawFd;
+use std::os::unix::prelude::OsStrExt;
 use std::path::PathBuf;
 use std::process::exit;
 use std::process::Command;
@@ -24,6 +26,17 @@ fn cache_dir() -> PathBuf {
     return PathBuf::from(".nix-eval-cache");
 }
 
+fn get_cache_key(args: &[String]) -> Result<String, std::io::Error> {
+    let mut hasher = Blake2s256::new();
+    for arg in &args[1..] {
+        hasher.update(arg);
+    }
+    hasher.update(env::current_dir()?.as_os_str().as_bytes());
+    let res = hasher.finalize();
+    Ok(hex::encode(res))
+}
+
+
 // TODO: better error handling and nicer error messages...
 fn main() -> Result<(), std::io::Error> {
     let args: Vec<_> = env::args().collect();
@@ -35,28 +48,24 @@ fn main() -> Result<(), std::io::Error> {
         eprintln!("USAGE: {} NIX_COMMAND...", args[0]);
         exit(1);
     }
+    let cache_key = get_cache_key(&args[2..])?;
 
     let dir = cache_dir();
     fs::create_dir_all(&dir)?;
 
-    let mut write_file = NamedTempFile::new_in(&dir)?;
+    let write_file = NamedTempFile::new_in(&dir)?;
 
     fcntl(
         write_file.as_raw_fd(),
         FcntlArg::F_SETFD(FdFlag::empty()),
     )?;
-    write_file.write_all(b"foo")?;
-    //println!("pause!");
-    //unistd::pause();
 
     let status = Command::new(&args[1])
         .args(&args[2..])
         .env("LD_PRELOAD", library)
         .env("NIX_EVAL_CACHE_FD", write_file.as_raw_fd().to_string())
         .status()?;
-    // TODO, hash this in future
-    let path = format!("{}", env::current_dir()?.display())[1..].replace("/", "-");
-    write_file.persist(dir.join(path))?;
+    write_file.persist(dir.join(cache_key))?;
 
     match status.code() {
         Some(code) => exit(code),
